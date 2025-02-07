d18adc9a87048d30ce82291ec1f195018b688b73
Kelamaarthi <kelamaarthi@gmail.com>
2025-02-07 16:45:13 +0530

    Added add.py

---
+++ add.py
@@ -0,0 +1,12 @@
+# Program to add two numbers
+
+# Taking input from the user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Adding the two numbers
+sum = num1 + num2
+
+# Displaying the result
+print("The sum is:", sum)
+
